6f8bc6bf8fe76757c20bfa2d468bb3b98418ddb1
andresqb198 <andresqb198@gmail.com>
2024-09-11 21:21:46 -0500

    First steps in C++

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # competitive-programming-handbook
+
+For run code:
+```sh
+g++ -std=c++11 -O2 -Wall test.cpp -o test.out
+```
+Where:
+- -std=c++11 is the compiler C++11
+- -O2 optimizes the code
+- -Wall shows warnings about possible errors
---
+++ hola.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    string x;
+    cin >> a >> b >> x; // cin for input and cout for output
+    cout << "La suma de " << a << " + " << b << " es: " << a+b << " " << x << "\n";
+}
